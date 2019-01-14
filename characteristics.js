f450ea0ffe128fd1112ec95853c6ca47b3f2d16c
Craig Woolgar <craig.woolgar@seequent.com>
2019-01-15 09:36:29 +1300

    Push Height Weight

---
+++ characteristics.js
@@ -0,0 +1,227 @@
+var data = {
+    "human": {
+        "base_height_ft": 4,
+        "base_height_in": 8,
+        "base_weight_lb": 110,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+    "dwarf, hill": {
+        "base_height_ft": 3,
+        "base_height_in": 8,
+        "base_weight_lb": 115,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 4,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "dwarf, mountain": {
+        "base_height_ft": 4,
+        "base_height_in": 0,
+        "base_weight_lb": 130,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 4,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "elf, high": {
+        "base_height_ft": 4,
+        "base_height_in": 6,
+        "base_weight_lb": 90,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 4
+    },
+    "elf, wood": {
+        "base_height_ft": 4,
+        "base_height_in": 6,
+        "base_weight_lb": 100,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 4
+    },
+    "elf, drow": {
+        "base_height_ft": 4,
+        "base_height_in": 5,
+        "base_weight_lb": 75,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 6,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 6
+    },
+    "half-orc": {
+        "base_height_ft": 4,
+        "base_height_in": 10,
+        "base_weight_lb": 140,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "tiefling": {
+        "base_height_ft": 4,
+        "base_height_in": 9,
+        "base_weight_lb": 110,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 8,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+    "gnome": {
+        "base_height_ft": 2,
+        "base_height_in": 11,
+        "base_weight_lb": 35,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 4,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 1
+    },
+    "halfling": {
+        "base_height_ft": 2,
+        "base_height_in": 7,
+        "base_weight_lb": 35,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 4,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 1
+    },
+    "aasimar": {
+        "base_height_ft": 4,
+        "base_height_in": 8,
+        "base_weight_lb": 110,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+    "bugbear": {
+        "base_height_ft": 6,
+        "base_height_in": 0,
+        "base_weight_lb": 200,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 12,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "firbolg": {
+        "base_height_ft": 6,
+        "base_height_in": 2,
+        "base_weight_lb": 175,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 12,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "goblin": {
+        "base_height_ft": 3,
+        "base_height_in": 5,
+        "base_weight_lb": 35,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 4,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 1
+    },
+    "goliath": {
+        "base_height_ft": 6,
+        "base_height_in": 2,
+        "base_weight_lb": 200,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "hobgoblin": {
+        "base_height_ft": 4,
+        "base_height_in": 8,
+        "base_weight_lb": 11,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+    "kenku": {
+        "base_height_ft": 4,
+        "base_height_in": 4,
+        "base_weight_lb": 50,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 8,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 6
+    },
+    "kobold": {
+        "base_height_ft": 2,
+        "base_height_in": 1,
+        "base_weight_lb": 25,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 4,
+        "weight_x_dice_cnt": 1,
+        "weight_x_dice_face": 1
+    },
+    "lizardfolk": {
+        "base_height_ft": 4,
+        "base_height_in": 9,
+        "base_weight_lb": 120,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "orc": {
+        "base_height_ft": 5,
+        "base_height_in": 4,
+        "base_weight_lb": 175,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 8,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "tabaxi": {
+        "base_height_ft": 4,
+        "base_height_in": 10,
+        "base_weight_lb": 90,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+    "triton": {
+        "base_height_ft": 4,
+        "base_height_in": 6,
+        "base_weight_lb": 90,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+    "yuan-ti": {
+        "base_height_ft": 4,
+        "base_height_in": 8,
+        "base_weight_lb": 110,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 10,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+    "dragonborn": {
+        "base_height_ft": 5,
+        "base_height_in": 6,
+        "base_weight_lb": 175,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 8,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 6
+    },
+    "half-elf": {
+        "base_height_ft": 4,
+        "base_height_in": 9,
+        "base_weight_lb": 100,
+        "height_x_dice_cnt": 2,
+        "height_x_dice_face": 8,
+        "weight_x_dice_cnt": 2,
+        "weight_x_dice_face": 4
+    },
+}
